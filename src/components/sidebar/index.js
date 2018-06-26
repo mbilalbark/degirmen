539c1877c0c0b824e853d016799867d12d980809
mbilalbark <muh.bark@gmail.com>
2018-06-26 04:13:16 +0300

    SideBar güncelleme

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -6,12 +6,19 @@ class Siderbar extends React.Component {
  
 render () {
     return (
-    <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+    <Sider breakpoint="lg"
+    style ={{height: '100vh'}}
+    collapsedWidth="0"
+    onCollapse={(collapsed, type) => { console.log(collapsed, type); }}>
       <div style={{ height: 32, background:'#0033cc', margin: 16}}/>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
         <Menu.Item key="1">
           <Icon type="user" />
           <span className="nav-text">Müşteri Ekle</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="barcode" />
+          <span className="nav-text">Fatura</span>
         </Menu.Item>
       </Menu>
     </Sider>
