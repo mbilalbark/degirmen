b8954daa1125a958b09ab6f9179133ee63f7b3d9
mbilalbark <muh.bark@gmail.com>
2018-06-28 02:38:42 +0300

    Muşteri tablolama istek  kısmı yapılmıştır.

--- src/components/customerTable/index.js
+++ src/components/customerTable/index.js
@@ -1,31 +1,41 @@
 import React, {Componenet} from 'react'
 import { Popconfirm, Table } from 'antd'
+import axios from 'axios';   
+import { SERVERURL } from '../../config/'
 class CustomerTable extends React.Component {
    
     constructor(props) {
         super(props);
         this.columns = [{
+            key : 1,
             title: 'Şirket Adı',
             dataIndex: 'sirketAdi',
         },{
+            key : 2,
             title: 'Adres',
             dataIndex: 'adres',
         },{
+            key : 3,
             title: 'Vergi Dairesi',
             dataIndex: 'vergiDairesi',
           },{
+            key : 4,
             title: 'Hesap No',
             dataIndex: 'hesapNo',
           },{
+            key : 5,
             title: 'Telefon',
             dataIndex: 'telefon',
           },{
+            key : 6, 
             title: 'Faks',
             dataIndex: 'faks',
           },{
+            key : 7,
             title: 'Banka Adı',
             dataIndex: 'bankaAdi',
           },{
+            key : 8,
             title: 'Bakiye',
             dataIndex: 'bakiye',
         }, {
@@ -42,11 +52,27 @@ class CustomerTable extends React.Component {
             );
           },
         }]
-        
         this.state = {
             dataSource: [],
           };
         }
+     
+    componentWillMount () {
+      axios({
+        method: 'post',
+        url: `${SERVERURL}degirmen-api/api/customer.php`,
+        headers : {
+          'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+        data: {
+         customerList:"list"
+        }
+      })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+    }
 
     render() {
         const { dataSource } = this.state;
