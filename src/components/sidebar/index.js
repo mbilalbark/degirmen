7157895442c90683421d99fcb635023fd2ad5f5a
mbilalbark <muh.bark@gmail.com>
2018-06-27 04:10:23 +0300

    Sidebara link eklenmesi

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import { Link } from "react-router-dom";
 const { Sider } = Layout;
 
 class Siderbar extends React.Component {
@@ -14,11 +15,11 @@ render () {
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
         <Menu.Item key="1">
           <Icon type="user" />
-          <span className="nav-text">Müşteri Ekle</span>
+          <span className="nav-text"><Link to="/customers" style={{color:'white'}}>Müşteriler</Link></span>
         </Menu.Item>
         <Menu.Item key="2">
           <Icon type="barcode" />
-          <span className="nav-text">Fatura</span>
+          <span className="nav-text"><Link to="/product" style={{color:'white'}}>Fatura</Link></span>
         </Menu.Item>
       </Menu>
     </Sider>
