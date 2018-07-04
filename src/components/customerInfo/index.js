e04ab4a800986299979619dfd1d686b1d015c29a
mbilalbark <muh.bark@gmail.com>
2018-07-04 14:38:48 +0300

    Müşteri bilgi düzenlendi

--- src/components/customerInfo/index.js
+++ src/components/customerInfo/index.js
@@ -1,30 +1,34 @@
 import React, {Componenet} from 'react'
-import { Form, Icon, Input, Button, Spin } from 'antd'
-
+// import { Form, Icon, Input, Button, Spin } from 'antd'
+import moment from 'moment'
 class CustomerInfo extends React.Component {
+  state ={
+      sirketAdi:null
+  }
+
 
     render () {
         return (
-            <div style={{float:'none'}}>
+            <div style={{float:'none'}} >
                 <div style={{float:'left',paddingLeft:20}} >
-                 <p> Müşteri Adı   </p> 
-                 <p> bilal </p>
+                 <h3> Müşteri Adı   </h3> 
+                 <p > {this.props.must.sirketAdi} </p>
                 </div>
                 <div style={{ paddingLeft:500}} > 
-                 <p> Tarih </p>
-                 <p> 02/10/2018 </p>
+                 <h3> Tarih </h3>
+                 <p> {moment().format('L')} </p>
                 </div>
                 <div style={{paddingLeft:20}}> 
-                    <p> Adres </p> 
-                    <p> Bağlarbaşı mah 452. sok 14/11 </p>
+                    <h3> Adres </h3> 
+                    <p> {this.props.must.adres} </p>
                 </div>
                 <div style={{float:'left', paddingLeft:20}}>  
-                     <p> Vergi Dairesi </p> 
-                     <p> 0123456789 </p> 
+                    <h3> Vergi Dairesi </h3>  
+                    <p> {this.props.must.vergiDairesi} </p> 
                 </div>
                 <div style={{paddingLeft:200}}>
-                 <p>Hesap No </p>
-                 <p> 0123456789 </p> 
+                 <h3>Hesap No </h3>
+                 <p> {this.props.must.hesapNo} </p> 
                 </div>
             </div>
         )
