8679104a55fcf4ef5d86fe324a0028b4c7c7b161
mbilalbark <muh.bark@gmail.com>
2018-07-19 05:51:45 +0300

    Fatura düzenleme ekleme ve silme işlemi yapılmıştır.

--- src/container/productSale/index.js
+++ src/container/productSale/index.js
@@ -21,7 +21,12 @@ class ProductSale extends React.Component {
       adres:'',
       vergiNumarasi:''
     },
-    count:1
+    count:1,
+    Invoice:{
+      fiyat:0,
+      kdvToplam:0,
+      yekun:0
+    }
   }
 
   info = (customer, prod, amount) => {
@@ -29,13 +34,24 @@ class ProductSale extends React.Component {
     prod.miktar = amount
     prod.key = prod.Id
     console.log(prod)
+    this.state.Invoice.fiyat += prod.fiyat * amount
+    this.state.Invoice.kdvToplam += ( prod.fiyat * amount / 100 ) * prod.kdv
+    this.state.Invoice.yekun += ( prod.fiyat * amount / 100 ) * prod.kdv + prod.fiyat * amount 
+    this.setState({Invoice:this.state.Invoice})
+    console.log('fatura',this.state.Invoice)
     this.setState({ product: [...this.state.product, prod], count: this.state.count + 1  })  
-    this.setState({customer:customer})
+    this.setState({customer:customer})    
   }
-  
+  // del
   onDelete = (id) => {
     console.log(id)
     const result = this.state.product.filter(key => key.key!=id);
+    const res = this.state.product.filter(key => key.key==id)[0];
+    console.log('silinen', res.fiyat)
+    this.state.Invoice.fiyat -= res.toplam
+    this.state.Invoice.kdvToplam -= (res.toplam/100 * res.kdv)
+    this.state.Invoice.yekun -= ((res.toplam/100 * res.kdv) + res.toplam)
+    this.setState({Invoice:this.state.Invoice})
     this.setState({product:result})
   }
 
@@ -45,7 +61,7 @@ class ProductSale extends React.Component {
     <CostumerCall  onInfoClick = {this.info}/> 
     <CostumerInfo must={this.state.customer} />
     <InvoiceTable product={this.state.product} sil={this.onDelete}/>
-    <InvoiceInfo/>
+    <InvoiceInfo pro={this.state.Invoice}/>
   </div>
     );
   } 
